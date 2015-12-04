cfccb112128bdac4eadcf05742ca06035edd17ca
Anthony Renteria <rentant2@isu.edu>
2015-12-04 23:06:06 +0000

    added authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,5 @@ Rails.application.configure do
   config.assets.compile = true
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.default_url_options = { host: 'https://prepare-to-terminate-arenteria518.c9users.io/', port: 8080 }
 end
