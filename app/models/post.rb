11def55eb87853ab8562d99fe19fcf831d792859
Anthony Renteria <rentant2@isu.edu>
2015-12-05 21:25:22 +0000

    changed thumbnail to crop to 16:9 - fixed typo

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
     validates :title, presence: true, length: {minimum: 5}
     validates :body, presence: true
-    has_attached_file :image, styles: {thumbnail: "720x400#>", fullscreen: "1980X600#" }
+    has_attached_file :image, styles: {thumbnail: "720x400#", fullscreen: "1980X600#" }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
