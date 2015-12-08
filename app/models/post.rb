036588b0faefcf5f88bfa7205ca6c7c6ad99a6d5
Anthony Renteria <rentant2@isu.edu>
2015-12-08 20:02:10 +0000

    added vanity URLs

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ActiveRecord::Base
     validates :body, presence: true
     has_attached_file :image, styles: {thumbnail: "720x400#", fullscreen: "1980X600#" }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+    
+    def slug
+        title.downcase.gsub(" ", "-")
+    end
+    
+    def to_param 
+        "#{id}-#{slug}"
+    end
 end
